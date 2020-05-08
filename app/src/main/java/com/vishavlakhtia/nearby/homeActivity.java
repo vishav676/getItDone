014acb7b7614d30dfe6c3fd6c95cde2ab9685ceb
Vishav Lakhtia <37496498+vishav676@users.noreply.github.com>
2020-05-08 23:35:43 +0200

    Setting User Profile Pic
    Using FirebaseStorage and FirebaseAuth

--- app/src/main/java/com/vishavlakhtia/nearby/homeActivity.java
+++ app/src/main/java/com/vishavlakhtia/nearby/homeActivity.java
@@ -1,23 +1,41 @@
 package com.vishavlakhtia.nearby;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
 import com.vishavlakhtia.nearby.outline.Users;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -26,6 +44,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.ByteArrayOutputStream;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 import io.paperdb.Paper;
 
@@ -33,7 +53,11 @@ public class homeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     NavController navController;
+    String TAG = "HomeActivity";
     DataSnapshot dataSnapshot;
+    Uri imageUri;
+    CircleImageView profile;
+    int TAKE_IMAGE_CODE = 676;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,24 +66,33 @@ public class homeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Paper.init(this);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
+
+
         View headerView = navigationView.getHeaderView(0);
         TextView username = headerView.findViewById(R.id.username_profile);
-        CircleImageView profile = headerView.findViewById(R.id.profile_image);
+        profile = headerView.findViewById(R.id.profile_image);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivityForResult(intent,TAKE_IMAGE_CODE);
+                }
+            }
+        });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         FirebaseUser current = FirebaseAuth.getInstance().getCurrentUser();
         username.setText(current.getDisplayName());
+        if(current.getPhotoUrl()!=null) {
+
+            Picasso.get().load(current.getPhotoUrl().toString()).into(profile);
+        }
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
@@ -68,6 +101,90 @@ public class homeActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == TAKE_IMAGE_CODE && resultCode == RESULT_OK && data!=null)
+        {
+            /*CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getOriginalUri();
+            profile.setImageURI(imageUri);*/
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            profile.setImageBitmap(bitmap);
+
+            handleUpload(bitmap);
+
+            /*FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(user.getDisplayName())
+                    .setPhotoUri(imageUri).build();
+            user.updateProfile(profileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(),"done",Toast.LENGTH_SHORT).show();
+                }
+            });*/
+        }
+        else {
+            Toast.makeText(this, "Error, Try again", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this,homeActivity.class));
+            finish();
+        }
+    }
+    public void handleUpload(Bitmap bitmap)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        final StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("profileUser")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()+".jpeg");
+
+        reference.putBytes(baos.toByteArray())
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        getDownloadUrl(reference);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG,"onFailure: ",e.getCause());
+                    }
+                });
+    }
+
+
+    public void getDownloadUrl(StorageReference reference){
+        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d(TAG,"onSucess: "+uri);
+                setUserProfileUrl(uri);
+            }
+        });
+    }
+
+    public void setUserProfileUrl(Uri uri)
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                .setDisplayName(user.getDisplayName())
+                .setPhotoUri(uri).build();
+        user.updateProfile(profileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(getApplicationContext(),"done",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
